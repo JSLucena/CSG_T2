9773279ab280850b2ed864995c9fd71335ea7861
Joaquim <joca.lucena@gmail.com>
2021-05-15 17:30:07 -0300

    Exploracao do openGL OK, classe Player OK, leitor de palette e de sprites OK, arte OK, proximo passo -> movimento do jogador

---
+++ RGB.h
@@ -0,0 +1,115 @@
+#ifndef RGB_hpp
+#define RGB_hpp
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+#ifdef WIN32
+#include <windows.h>
+#include <glut.h>
+#endif
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#endif
+
+#ifdef __linux__
+#include <glut.h>
+#endif
+
+#include "Ponto.h"
+#include <vector>
+
+class RGB{
+
+    public:
+
+    int r,g,b;
+
+    void imprime()
+
+    {
+
+        cout << "Cor: " << setw(4) << g << setw(4) << r << setw(4) << b;
+
+    }
+
+};
+
+class ModeloMatricial{
+
+    public:
+        int height;
+        int width;
+        int M[50][50];
+
+        void leModelo(const char *nome);
+
+        void desenhaModelo(float posX, float posY, RGB Palette[100]);
+
+};
+void ModeloMatricial::leModelo(const char *nome)
+{
+    ifstream input;
+    input.open(nome, ios::in);
+    if (!input)
+    {
+        cout << "Erro ao abrir " << nome << ". " << endl;
+        exit(0);
+    }
+    cout << "Lendo arquivo " << nome << "...";
+    string S;
+    unsigned int linhas, colunas;
+
+    input >> linhas >> colunas;
+
+    height = linhas;
+    width = colunas;
+    for (int i=0; i< linhas; i++)
+    {
+        for(int j = 0; j < colunas; j++)
+        {
+            double x,y;
+        // Le cada elemento da linha
+        input >> M[i][j];
+        if(!input)
+            break;
+        }
+
+    }
+    for (int i=0; i< linhas; i++)
+    {
+        for(int j = 0; j < colunas; j++)
+        {
+            cout << M[i][j] << " ";
+        }
+        cout << endl;
+
+    }
+
+
+    cout << "Modelo lido com sucesso!" << endl;
+
+}
+void ModeloMatricial::desenhaModelo(float posX, float posY, RGB Palette[100])
+{
+    float offsetX = 0, offsetY = 0;
+    for(int i = 0; i < height;i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            glColor3f(Palette[M[i][j]].r/255.0, Palette[M[i][j]].g/255.0, Palette[M[i][j]].b/255.0); // R, G, B  [0..1]
+            glRectf(posY-offsetY,posX+offsetX,posY-offsetY+1,posX+offsetX+1);
+            offsetY++;
+        }
+         offsetX--;
+         offsetY=0;
+    }
+}
+
+
+
+
+#endif
---
+++ player.h
@@ -0,0 +1,91 @@
+#ifndef player_hpp
+#define player_hpp
+
+#include <iostream>
+using namespace std;
+
+
+#ifdef WIN32
+#include <windows.h>
+#include <glut.h>
+#endif
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#endif
+
+#ifdef __linux__
+#include <glut.h>
+#endif
+
+#include "Poligono.h"
+#include "RGB.h"
+#include <vector>
+
+class Player
+{
+
+    float speed;
+    int direction;
+    float shotAngle;
+    int shotPower;
+    ModeloMatricial Sprite;
+    Poligono Hitbox;
+    float posX,posY;
+public:
+    Player(float startPosX, float startPosY);
+
+    void setspeed(float f){
+        speed = f;
+    };
+    float getspeed(){ return speed; };
+
+    void setDirection(int i){
+        direction = i;
+    };
+    int getDirection(){ return direction; };
+
+    void setShotAngle(float f){
+        shotAngle = f;
+    };
+    float getShotAngle(){ return shotAngle;};
+
+    void setShotPower(int i){
+        shotPower = i;
+    };
+    int getShotPower() {return shotPower;};
+
+
+    void drawSprite();
+    Poligono getHitbox(){ return Hitbox; };
+
+    void updateHitbox();
+
+
+};
+Player::Player(float startPosX, float startPosY)
+{
+    Sprite.leModelo("./sprites/player");
+    speed = 0;
+    direction = 0;
+    shotAngle = 0;
+    shotPower = 0;
+    posX = startPosX;
+    posY = startPosY;
+
+}
+void Player::updateHitbox()
+{
+    Hitbox = Poligono();
+    Hitbox.insereVertice(Ponto(posX,posY));
+    Hitbox.insereVertice(Ponto(posX+Sprite.width,posY));
+    Hitbox.insereVertice(Ponto(posX+Sprite.width,posY-Sprite.height));
+    Hitbox.insereVertice(Ponto(posX,posY-Sprite.height));
+    Hitbox.imprime();
+}
+
+
+
+
+
+#endif
